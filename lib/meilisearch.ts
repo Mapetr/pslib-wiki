272b522c85fec5b21168d34a5e21da8551d8fb4e
mapetr <info@mapetr.moe>
2025-03-04 01:12:04 +0100

    init react project

--- lib/meilisearch.ts
+++
@@ -1,7 +0,0 @@
-import { MeiliSearch } from "meilisearch";
-
-export const searchClient = new MeiliSearch({
-  host: process.env.MEILI_HOST ?? "http://meilisearch:7700",
-  apiKey: process.env.MEILI_MASTER_KEY,
-});
-export const DocumentsIndex = searchClient.index("documents");
